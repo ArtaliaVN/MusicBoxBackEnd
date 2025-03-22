b9613ef6e8c33cb352778274832db23e8a6d80d0
Nguyen Minh Thong <Smart.Nguyen.0411@gmail.com>
2025-03-22 12:12:03 +0700

    22/3/2025

---
+++ EurekaClientService/src/test/java/com/example/Artalia/ArtaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Artalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
